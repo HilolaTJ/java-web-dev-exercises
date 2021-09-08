29f0d75964d78c0feb7217f334009d6f1f500e6f
Hilola <tursun.hilola@gmail.com>
2021-09-08 12:17:07 -0500

    Studio 1 done

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,56 @@
+package org.launchcode.java.studios.areaofacircle;
+
+// 1. Ask the user for the radius of a circle
+// 2. Calculate its area
+// 3. Print the result
+
+import java.util.Scanner;
+
+public class Area {
+
+    public static void main(String[] args){
+
+        // Declare class variables here
+
+        Area area = new Area();
+        area.calculateArea();
+    }
+
+    // Create Constructor here
+    public Area(){
+
+    }
+
+    // Create other methods here
+    public  void calculateArea() {
+        // Ask for the radius
+        Scanner input = new Scanner(System.in);
+        try {
+            boolean keepOnLooping = true;
+            while(keepOnLooping) {
+                Double myUsersRadius = input.nextDouble();
+
+                if (myUsersRadius < 0) {
+                    // print an error to the user
+                    // Re-prompt the user
+                    System.out.println("You can not enter a negative number.");
+                } else {
+                    // continue as normal
+                    // Calculate the area
+                    Double area = Circle.getArea(myUsersRadius);
+
+                    // Print the result
+                    System.out.println(area);
+
+                    // Tell the while loop to exit
+                    keepOnLooping = false;
+                }
+            }
+        } catch(Exception error) {
+            System.out.println("You must provide a numeric input.");
+        }
+
+        // Close the scanner
+        input.close();
+    }
+}
---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,17 @@
+package org.launchcode.java.studios.areaofacircle;
+
+// 1. Create a getArea method
+// 2. Take in a radius that is a Double
+// 3. Return the area
+
+public class Circle {
+    // Declare class variables at the top
+
+    // Create a constructor
+    public Circle(){}
+
+    // Create our methods
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius;
+    }
+}
